e7a48d8815c65d000c1d2782efdc887c4a39d710
edward <edwa.joha@gmail.com>
2026-02-07 06:17:26 +0100

    stats

--- backend/app/funcs/querys.js
+++ backend/app/funcs/querys.js
@@ -1,26 +1,37 @@
 import db from "../db.js";
 
 export function insert_coords(lat, long) {
-    try {
-        if (!lat || !long) return false;
+	try {
+		if (!lat || !long) return false;
 
-        const q = db.prepare("INSERT INTO coords (id, lat, long, date) VALUES (?, ?, ?, ?)");
-        q.run(crypto.randomUUID(), lat, long, new Date().getTime());
+		const q = db.prepare("INSERT INTO coords (id, lat, long, date) VALUES (?, ?, ?, ?)");
+		q.run(crypto.randomUUID(), lat, long, new Date().getTime());
 
-        return true;
-    } catch (error) {
-        return false;
-    }
+		return true;
+	} catch (error) {
+		return false;
+	}
 }
 
 export function get_coords() {
-    try {
-        const q = db.prepare("SELECT * FROM coords ORDER BY date DESC LIMIT 4");
+	try {
+		const q = db.prepare("SELECT * FROM coords ORDER BY date DESC LIMIT 4");
 
-        const coords = q.all();
+		const coords = q.all();
 
-        return coords;
-    } catch (error) {
-        return [];
-    }
+		return coords;
+	} catch (error) {
+		return [];
+	}
+}
+
+export function increment_stat(type) {
+	try {
+		const q = db.prepare(`UPDATE stats SET ${type} = ${type} + 1 WHERE id = 1`);
+		q.run();
+		return true;
+	} catch (error) {
+		console.log("stats fel: ");
+		return false;
+	}
 }
---
+++ backend/app/funcs/stats_middleware.js
@@ -0,0 +1,8 @@
+import { increment_stat } from "./querys.js";
+
+export function stats_middleware(type) {
+	return (req, res, next) => {
+		increment_stat(type);
+		next();
+	};
+}
