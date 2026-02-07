75785ebd819ad35b3fcd12f81b9b934a48511ff3
edward <edwa.joha@gmail.com>
2026-02-07 03:21:38 +0100

    pins

--- frontend/src/components/Map_box.jsx
+++ frontend/src/components/Map_box.jsx
@@ -1,20 +1,52 @@
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
-import { useEffect } from "react";
+import { MapContainer, CircleMarker, TileLayer, useMap, Marker } from "react-leaflet";
+import { useEffect, useRef } from "react";
+import Pin_icon from "../icons/Pin_icon";
 
 export default function Map_box({ position }) {
+	const marker_pos = [position.lat, position.long];
+
+	const map_ref = useRef(null);
+
+	function onclick() {
+		map_ref.current.panTo(marker_pos);
+	}
+
 	return (
 		<MapContainer
+			ref={map_ref}
 			center={[position.lat, position.long]}
+			zoomControl={false}
 			zoom={15}
 			scrollWheelZoom={false}
-			style={{ height: "100%", width: "100%", zIndex: 2, borderRadius: "6px" }}
+			attributionControl={false}
+			style={{
+				height: "100%",
+				width: "100%",
+				zIndex: 2,
+				borderRadius: "6px",
+				backgroundColor: "#1b1b1b",
+			}}
 		>
 			<TileLayer
 				url={`https://{s}.tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token=${"y79r0d3305U62Jkfz2tGHQq2Qmuea0rxf2YIBeeJbTjqvQ2yuyxHZJi0fMANL8MZ"}`}
 				attribution='&copy; <a href="http://jawg.io">Jawg</a>'
 			/>
 			<Map_component position={position} />
+			{/* <CircleMarker
+				center={[position.lat, position.long]}
+				radius={12}
+				fillColor="#90D5FF"
+				opacity={1}
+				color="#1E90FF"
+			/> */}
+
+			<Marker
+				position={[position.lat, position.long]}
+				eventHandlers={{
+					click: onclick,
+				}}
+			/>
 		</MapContainer>
 	);
 }
