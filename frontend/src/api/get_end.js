2b69d5cca9fdc6ac0f78a4896dd7fc3313d53201
edward <edwa.joha@gmail.com>
2026-02-07 05:30:33 +0100

    asd

--- frontend/src/api/get_end.js
+++ frontend/src/api/get_end.js
@@ -1,19 +1,23 @@
-import { base_url } from "./base_url";
+import { base_url } from "./base_url.js";
 
 async function get_end() {
-    try {
-        const res = await fetch(`${base_url}/api/get_end`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
+	try {
+		const res = await fetch(`${base_url}/api/get_end`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
 
-        return res.json();
-    } catch (error) {
-        console.log(error);
-        return null;
-    }
+		console.log(res);
+
+		if (!res.ok) throw "knas";
+
+		return res.json();
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
 }
 
 export default get_end;
