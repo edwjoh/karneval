3d34c0e66949bcc30fcdc09f190bddd930e6d89f
joeloster <joel.osterdahl16@gmail.com>
2026-02-07 16:47:53 +0100

    asd

--- backend/app/funcs/middlewares/stats_middleware.js
+++ backend/app/funcs/middlewares/stats_middleware.js
@@ -1,8 +1,8 @@
-import { increment_stat } from "./querys.js";
+import { increment_stat } from "../querys.js";
 
 export function stats_middleware(type) {
-	return (req, res, next) => {
-		increment_stat(type);
-		next();
-	};
+    return (req, res, next) => {
+        increment_stat(type);
+        next();
+    };
 }
--- frontend/src/pages/Main_page.jsx
+++ frontend/src/pages/Main_page.jsx
@@ -6,99 +6,108 @@ import Map_box from "../components/Map_box";
 import Loader from "../components/Loader";
 import Check_icon from "../icons/Check_icon";
 import X_icon from "../icons/X_icon";
+import coords_distance from "../misc/distance";
 
 function Main_page() {
-	const default_text = "Är du i slutet av kön? Dela din position";
+    const default_text = "Är du i slutet av kön? Hjälp andra och dela din position";
 
-	const [submit_content, set_submit_content] = useState(default_text);
-	const [is_loading_end_position, set_is_loading_end_position] = useState(false);
+    const [submit_content, set_submit_content] = useState(default_text);
+    const [is_loading_end_position, set_is_loading_end_position] = useState(false);
 
-	const [user_position, set_user_position] = useState({
-		lat: 55.7029,
-		long: 13.1947,
-	});
+    const [user_position, set_user_position] = useState({
+        lat: 55.7029,
+        long: 13.1947,
+    });
 
-	const [end_position, set_end_position] = useState({
-		lat: 55.7029,
-		long: 13.1947,
-	});
+    const [end_position, set_end_position] = useState({
+        lat: 55.7029,
+        long: 13.1947,
+    });
 
-	async function load_end_position() {
-		const data = await get_end();
+    function is_in_range() {
+        const MAX_DISTANCE = 250;
 
-		if (!data) return;
+        const distance = coords_distance(user_position, end_position);
+        console.log(distance);
 
-		set_end_position(data);
-		set_is_loading_end_position(false);
-	}
+        return distance < MAX_DISTANCE;
+    }
 
-	async function post_position() {
-		set_submit_content(<Loader color="black" size="24px" />);
+    async function load_end_position() {
+        const data = await get_end();
 
-		const res = await submit_end(user_position);
+        if (!data) return;
 
-		const message = res ? <Check_icon /> : <X_icon />;
+        set_end_position(data);
+        set_is_loading_end_position(false);
+    }
 
-		setTimeout(() => {
-			set_submit_content(message);
-			setTimeout(() => set_submit_content(default_text), 2000);
-		}, 1000);
-	}
+    async function post_position() {
+        set_submit_content(<Loader color="black" size="24px" />);
 
-	function position_setter() {
-		navigator.geolocation.getCurrentPosition((pos) => {
-			set_user_position({ lat: pos.coords.latitude, long: pos.coords.longitude });
-		});
-	}
+        const res = await submit_end(user_position);
 
-	useEffect(() => {
-		position_setter();
-		const interval_id = setInterval(() => {
-			position_setter();
-		}, 5000);
+        const message = res ? <Check_icon /> : <X_icon />;
 
-		return () => clearInterval(interval_id);
-	}, []);
+        setTimeout(() => {
+            set_submit_content(message);
+            setTimeout(() => set_submit_content(default_text), 2000);
+        }, 1000);
+    }
 
-	useEffect(() => {
-		load_end_position();
-		const interval_id = setInterval(() => {
-			load_end_position();
-		}, 120000);
+    function position_setter() {
+        navigator.geolocation.getCurrentPosition((pos) => {
+            set_user_position({ lat: pos.coords.latitude, long: pos.coords.longitude });
+        });
+    }
 
-		return () => clearInterval(interval_id);
-	}, []);
+    useEffect(() => {
+        position_setter();
+        const interval_id = setInterval(() => {
+            position_setter();
+        }, 5000);
 
-	return (
-		<div className="flex flex-col gap-4 p-4 h-full">
-			<div className="w-full h-full rounded relative">
-				{is_loading_end_position && <Loader_display />}
+        return () => clearInterval(interval_id);
+    }, []);
 
-				<Map_box user_position={user_position} end_position={end_position} />
-			</div>
+    useEffect(() => {
+        load_end_position();
+        const interval_id = setInterval(() => {
+            load_end_position();
+        }, 120000);
 
-			<button
-				disabled={submit_content != default_text}
-				onClick={() => post_position()}
-				className="p-4 rounded w-full bg-green-300 flex items-center justify-center"
-			>
-				{submit_content}
-			</button>
-		</div>
-	);
+        return () => clearInterval(interval_id);
+    }, []);
+
+    return (
+        <div className="flex flex-col gap-4 p-4 h-full">
+            <div className="w-full h-full rounded relative">
+                {is_loading_end_position && <Loader_display />}
+
+                <Map_box user_position={user_position} end_position={end_position} />
+            </div>
+            <button
+                disabled={!is_in_range() || submit_content != default_text}
+                onClick={() => post_position()}
+                className="p-4 rounded w-full bg-green-300 flex items-center justify-center disabled:opacity-30"
+            >
+                {submit_content}
+            </button>
+        </div>
+    );
 }
 
 export default Main_page;
 
 function Loader_display() {
-	return (
-		<div
-			onClick={(e) => {
-				e.stopPropagation();
-			}}
-			className="bg-black/50 w-full h-full flex justify-center items-center z-20 absolute top-0 left-0"
-		>
-			<Loader color="white" size="48px" />
-		</div>
-	);
+    return (
+        <div
+            onClick={(e) => {
+                e.stopPropagation();
+            }}
+            className="bg-black/50 w-full h-full flex justify-center items-center z-20 absolute top-0 left-0"
+        >
+            <Loader color="white" size="48px" />
+        </div>
+    );
 }
